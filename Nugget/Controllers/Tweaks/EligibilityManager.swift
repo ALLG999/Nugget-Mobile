082ac1e9b7facc1f9f2af8cf4d6f2409004410b3
托尼老师五毛钱洗剪吹 <136478738@qq.com>
2024-12-18 11:16:02 +0800

    Update EligibilityManager.swift

--- Nugget/Controllers/Tweaks/EligibilityManager.swift
+++ Nugget/Controllers/Tweaks/EligibilityManager.swift
@@ -35,7 +35,7 @@ class EligibilityManager: ObservableObject {
             if var plist = String(data: data, encoding: .utf8) {
                 // apply the region code
                 if let regionCode = Locale.current.regionCode {
-                    print("Applying for region code: \(regionCode)")
+                    print("申请区域代码： \(regionCode)")
                     plist = plist.replacingOccurrences(of: "US", with: regionCode)
                 }
                 return plist.data(using: .utf8) ?? Data()
